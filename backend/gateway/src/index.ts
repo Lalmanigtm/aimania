4842f96a2c7e6bb15977b9f79e3eb9a42a7874a7
Saurab Gautam <missionprojects250@gmail.com>
2026-07-22 08:43:20 +0545

    03_setup user model

--- backend/gateway/src/index.ts
+++ backend/gateway/src/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import proxy from "express-http-proxy"
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+app.use("/auth", proxy(process.env.AUTH_SERVICES as string))
+
 app.get("/", (req, res) => {
-  res.status(200).json({message: "Hello, World!"});
+  res.status(200).json({ message: "Hello, World!" });
 })
 
 app.listen(PORT, () => {
