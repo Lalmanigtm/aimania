c96a0380dfb94ecc00bfd414bb95e1165855ca06
Saurab Gautam <missionprojects250@gmail.com>
2026-07-22 15:37:30 +0545

    08-setupping backend inside auth

--- backend/services/auth/src/index.ts
+++ backend/services/auth/src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import router from "./routes/auth.route.js";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+
+app.use("/", router)
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello, World! from auth service" });
